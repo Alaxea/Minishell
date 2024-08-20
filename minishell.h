b6c2fa5604771088ef98479e04e103c83af8fb6a
tsetsuko <zosia.gba@gmail.com>
2024-08-20 12:17:59 +0200

    library updated

--- minishell.h
+++ minishell.h
@@ -9,4 +9,29 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
+typedef enum	s_quote_mode
+{
+	DEFAULT,
+	SINGLE_Q,
+	DOUBLE_Q
+}	t_quote_mode;
+
+typedef enum	s_type
+{
+	WORD,
+	NEWLINE,
+	OPERATOR
+}	t_type;
+
+// tokens (nodes) -> for tokenization input (first step lexing)
+// "The shell breaks the input into tokens: words and operators" - Shell Command Language
+
+typedef struct s_token 
+{
+	t_token	*next;
+	t_token	*prev;
+	char	*value;
+	int		data_type;
+}	t_token;
+
 #endif
