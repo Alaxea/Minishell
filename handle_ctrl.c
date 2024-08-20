044782c2434ffc7300c27e1bb41543c359c1dfdd
Alicja Stefanska <alicja.stefanska44@gmail.com>
2024-08-20 16:11:54 +0200

    signal

--- handle_ctrl.c
+++ handle_ctrl.c
@@ -11,16 +11,39 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+
 /*handle ctrl-c*/
-void  handle_sigint(int sig)
+/*void  handle_sigint(int sig)
 {
 	(void)sig;
-	printf("\nminishell>> ");
-}
+
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
+}*/
 /*handle ctrl-\*/
+void	do_nothing(int sig)
+{
+	if (sig == 2)
+		printf("\n");
+}
+
+void	handle_sigint(int sig)
+{
+	(void)sig;
+	if (sig == 2)
+	{
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		write(1, "\n", 1);
+		rl_redisplay();
+	}
+}
 void handle_sigquit(int sig)
 {
     (void)sig;
+	do_nothing(sig);
 }
 
 int main()
