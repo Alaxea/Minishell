50c9339026008c611e78d13989e473782dd9bbbe
Alicja Stefanska <alicja.stefanska44@gmail.com>
2024-11-07 16:05:40 +0100

    norme

--- meat/utils/clean.c
+++ meat/utils/clean.c
@@ -12,84 +12,83 @@
 
 #include "../../minishell.h"
 
-void    clear_tab(char **tab)
+void	clear_tab(char **tab)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (tab)
-    {
-        while (tab[i])
-        {
-            free(tab[i]);
-            tab[i] = NULL;  // Dodane zerowanie
-            i++;
-        }
-        free(tab);
-        tab = NULL;  // Dodane zerowanie
-    }
+	i = 0;
+	if (tab)
+	{
+		while (tab[i])
+		{
+			free(tab[i]);
+			tab[i] = NULL;
+			i++;
+		}
+		free(tab);
+		tab = NULL;
+	}
 }
 
-void    clear_env(t_data *env)
+void	clear_env(t_data *env)
 {
-    if (!env)
-        return;
-    if (env->env_var)
-    {
-        clear_tab(env->env_var);
-        env->env_var = NULL;  // Dodane zerowanie
-    }
-    // Dodane sprawdzenie innych pól struktury env
-    if (env->envp)
-    {
-        clear_tab(env->envp);
-        env->envp = NULL;
-    }
+	if (!env)
+		return ;
+	if (env->env_var)
+	{
+		clear_tab(env->env_var);
+		env->env_var = NULL;
+	}
+	if (env->envp)
+	{
+		clear_tab(env->envp);
+		env->envp = NULL;
+	}
 }
 
-void    exit_shell(t_data *env, char *mess, int fail)
+void	exit_shell(t_data *env, char *mess, int fail)
 {
-    (void)mess;
-    if (!env)
-    {
-        if (fail)
-            exit(fail);
-        exit(EXIT_SUCCESS);
-    }
-    clear_env(env);  // Wyczyść env tylko raz
-    if (fail == -1)
-        exit(EXIT_FAILURE);
-    else if (!fail)
-        exit(EXIT_SUCCESS);
-    else
-        exit(fail);
+	(void)mess;
+	if (!env)
+	{
+		if (fail)
+			exit(fail);
+		exit(EXIT_SUCCESS);
+	}
+	clear_env(env);
+	if (fail == -1)
+		exit(EXIT_FAILURE);
+	else if (!fail)
+		exit(EXIT_SUCCESS);
+	else
+		exit(fail);
 }
 
-void    free_simple_cmd(t_simple_cmd *cmd) 
+void	free_simple_cmd(t_simple_cmd *cmd)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (!cmd)
-        return;
-    if (cmd->cmd) 
-    {
-       while (cmd->cmd[i])
-        {
-            free(cmd->cmd[i]);
-            i++;
-        }
-        free(cmd->cmd);
-    }
-    if (cmd->delimiter_heredoc)
-        free(cmd->delimiter_heredoc);
-    if (cmd->input_path)
-        free(cmd->input_path);
-    if (cmd->output_path)
-        free(cmd->output_path);
-    if (cmd->output_path_append)
-        free(cmd->output_path_append);
-    if (cmd->path) 
-        free(cmd->path);
-    free(cmd);
+	i = 0;
+	if (!cmd)
+		return ;
+	if (cmd->cmd)
+	{
+		while (cmd->cmd[i])
+		{
+			free(cmd->cmd[i]);
+			i++;
+		}
+		free(cmd->cmd);
+	}
+	if (cmd->delimiter_heredoc)
+		free(cmd->delimiter_heredoc);
+	if (cmd->input_path)
+		free(cmd->input_path);
+	if (cmd->output_path)
+		free(cmd->output_path);
+	if (cmd->output_path_append)
+		free(cmd->output_path_append);
+	if (cmd->path)
+		free(cmd->path);
+	free(cmd);
 }
--- meat/utils/env_helper.c
+++ meat/utils/env_helper.c
@@ -14,26 +14,28 @@
 
 void	copy_env_var(t_data *env, char **env_var)
 {
-	int i;
-	int size;
+	int	i;
+	int	size;
 
 	size = 0;
-	while(env_var[size])
+	while (env_var[size])
 		size++;
 	env->env_var = (char **)malloc(sizeof(char *) * (size + 1));
+	if (!env->env_var)
+		return ;
 	i = 0;
 	while (env_var[i])
 	{
 		env->env_var[i] = ft_strdup(env_var[i]);
 		i++;
 	}
-	env->env_var[i] = 0;
+	env->env_var[i] = NULL;
 }
 
 char	*set_env_var(t_data *env, char *name)
 {
-	int i;
-	size_t temp;
+	int		i;
+	size_t	temp;
 
 	i = 0;
 	while (env->env_var[i])
@@ -41,94 +43,95 @@ char	*set_env_var(t_data *env, char *name)
 		temp = ft_strchrn(env->env_var[i], '=');
 		if (ft_strncmp(env->env_var[i], name, ft_strlen(name)) == 0
 			&& temp == ft_strlen(name))
-				return (ft_strdup(env->env_var[i] + temp + 1));
+			return (ft_strdup(env->env_var[i] + temp + 1));
 		i++;
 	}
 	return (NULL);
 }
 
-void add_env_var(t_data *env, char *name, char *value) 
+void	add_env_var(t_data *env, char *name, char *value)
 {
-        if (!env || !name || !value)
-        	return;
+	char	*new_var;
+	char	*temp;
+	int		i;
+	char	**new_env;
 
-    char *new_var = ft_strjoin(name, "=");
-    if (!new_var)
-        return;
-    char *temp = ft_strjoin(new_var, value);
-    free(new_var);
-    if (!temp)
-        return;
-    delete_env_var(env, name);  // Remove existing variable if present
-    // Add new variable
-    int i = 0;
-    while (env->env_var[i])
-        i++;
-    char **new_env = malloc(sizeof(char *) * (i + 2));
-    if (!new_env)
-    {
-        free(temp);
-        return;
-    }
-    i = 0;
-    while (env->env_var[i])
-    {
-        new_env[i] = ft_strdup(env->env_var[i]);
-        i++;
-    }
-    new_env[i] = temp;
-    new_env[i + 1] = NULL;
-    clear_tab(env->env_var);
-    env->env_var = new_env;
-    env->envp = new_env;
+	if (!env || !name || !value)
+		return ;
+	new_var = ft_strjoin(name, "=");
+	if (!new_var)
+		return ;
+	temp = ft_strjoin(new_var, value);
+	free(new_var);
+	if (!temp)
+		return ;
+	delete_env_var(env, name);
+	i = 0;
+	while (env->env_var[i])
+		i++;
+	new_env = malloc(sizeof(char *) * (i + 2));
+	if (!new_env)
+	{
+		free(temp);
+		return ;
+	}
+	i = 0;
+	while (env->env_var[i])
+	{
+		new_env[i] = ft_strdup(env->env_var[i]);
+		i++;
+	}
+	new_env[i] = temp;
+	new_env[i + 1] = NULL;
+	clear_tab(env->env_var);
+	env->env_var = new_env;
+	env->envp = new_env;
 }
 
-void delete_env_var(t_data *env, char *name)
+void	delete_env_var(t_data *env, char *name)
 {
-    if (!env || !env->env_var || !name)
-        return;
+	int		var_count;
+	int		name_len;
+	int		i;
+	int		j;
+	char	**new_env;
 
-    int var_count = 0;
-    int name_len = ft_strlen(name);
-
-    // Count variables
-    while (env->env_var[var_count])
-        var_count++;
-
-    // Allocate new array
-    char **new_env = malloc(sizeof(char *) * (var_count + 1));
-    if (!new_env)
-        return;
-
-    int i = 0, j = 0;
-    while (env->env_var[i]) 
+	if (!env || !env->env_var || !name)
+		return ;
+	var_count = 0;
+	name_len = ft_strlen(name);
+	while (env->env_var[var_count])
+		var_count++;
+	new_env = malloc(sizeof(char *) * (var_count + 1));
+	if (!new_env)
+		return ;
+	i = 0;
+	j = 0;
+	while (env->env_var[i])
 	{
-        if (ft_strncmp(env->env_var[i], name, name_len) != 0 ||
-            (env->env_var[i][name_len] && env->env_var[i][name_len] != '=')) 
+		if (ft_strncmp(env->env_var[i], name, name_len) != 0
+			|| (env->env_var[i][name_len] && env->env_var[i][name_len] != '='))
 		{
-            new_env[j] = ft_strdup(env->env_var[i]);
-            if (!new_env[j]) 
+			new_env[j] = ft_strdup(env->env_var[i]);
+			if (!new_env[j])
 			{
-                // Cleanup on error
-                while (--j >= 0)
-                    free(new_env[j]);
-                free(new_env);
-                return;
-            }
-            j++;
-        }
-        i++;
-    }
-    new_env[j] = NULL;
-    // Free old environment
-    i = 0;
-    while (env->env_var[i]) 
+				while (--j >= 0)
+					free(new_env[j]);
+				free(new_env);
+				return ;
+			}
+			j++;
+		}
+		i++;
+	}
+	new_env[j] = NULL;
+	i = 0;
+	while (env->env_var[i])
 	{
-        free(env->env_var[i]);
-        i++;
-    }
-    free(env->env_var);
-    // Update both pointers
-    env->env_var = new_env;
-    env->envp = new_env;
+		free(env->env_var[i]);
+		i++;
+	}
+	free(env->env_var);
+	env->env_var = new_env;
+	env->envp = new_env;
 }
