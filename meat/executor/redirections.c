0b667cee6f4620082798e0228fd5ea1c49f7f15f
alicja <alicja@ThinkPad.>
2024-11-10 23:35:57 +0100

    heredoc

--- meat/executor/redirections.c
+++ meat/executor/redirections.c
@@ -6,7 +6,7 @@
 /*   By: alicja <alicja@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 14:30:42 by astefans          #+#    #+#             */
-/*   Updated: 2024/11/10 15:44:56 by alicja           ###   ########.fr       */
+/*   Updated: 2024/11/10 21:43:48 by alicja           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,7 +99,7 @@ static void	out_redir(t_simple_cmd *cmd)
 	close(fd_out);
 }
 
-static void	heredoc_redir(t_simple_cmd *cmd)
+/*static void	heredoc_redir(t_simple_cmd *cmd)
 {
 	int		fd;
 	char	*input;
@@ -144,8 +144,75 @@ static void	heredoc_redir(t_simple_cmd *cmd)
 	}
 	close(fd);
 	unlink(".heredoc");
+}*/
+static void handle_open_error(int fd, char *error_msg)
+{
+    if (fd < 0)
+    {
+        ft_putstr_fd(error_msg, 2);
+        exit(1);
+    }
 }
 
+static void handle_readline_error(char *input, int fd, char *error_msg)
+{
+    if (!input)
+    {
+        ft_putstr_fd(error_msg, 2);
+        close(fd);
+        exit(1);
+    }
+}
+
+static void write_and_free_input(char *input, int fd)
+{
+    ft_putstr_fd(input, fd);
+    ft_putstr_fd("\n", fd);
+    free(input);
+}
+
+static void handle_dup2_error(int fd, char *error_msg)
+{
+    if (dup2(fd, STDIN_FILENO) == -1)
+    {
+        ft_putstr_fd(error_msg, 2);
+        close(fd);
+        exit(1);
+    }
+}
+
+static void read_and_write_heredoc(int fd, t_simple_cmd *cmd)
+{
+    char *input;
+
+    while (1)
+    {
+        input = readline("> ");
+        handle_readline_error(input, fd, "Error: readline failed\n");
+        if (ft_strncmp(input, cmd->delimiter_heredoc,
+				ft_strlen(cmd->delimiter_heredoc)) == 0)
+        {
+            free(input);
+            break;
+        }
+        write_and_free_input(input, fd);
+    }
+}
+
+static void heredoc_redir(t_simple_cmd *cmd)
+{
+    int fd;
+
+    fd = open(".heredoc", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    handle_open_error(fd, "Error: failed to open heredoc file\n");
+    read_and_write_heredoc(fd, cmd);
+    close(fd);
+    fd = open(".heredoc", O_RDONLY);
+    handle_open_error(fd, "Error: failed to open heredoc file for reading\n");
+    handle_dup2_error(fd, "Error: failed to duplicate file descriptor\n");
+    close(fd);
+    unlink(".heredoc");
+}
 int	redir_check(t_simple_cmd *cmd)
 {
 	if (!cmd)
